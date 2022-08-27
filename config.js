ecdae059f6da15e7020664f2a07f0aace4549863
spyro-ser-ofc <spyroserofc@gmail.com>
2022-08-27 18:04:32 +0530

    spyro-bot

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/33683838312
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['33683838312','33683838312','33683838312','33683838312']
+global.premium = ['33683838312']
+global.packname = 'Hisoka Morrow'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Group Admin Special Features!',
+    botAdmin: 'Bots Must Be Admins First!',
+    owner: 'Owner Bot Special Features',
+    group: 'Feature Used For Group Only!',
+    private: 'Feature Used Only For Private Chat!',
+    bot: 'Bot Number User Special Feature',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
